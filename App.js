5d7e31b18bff5dbf640f9235d4125407a8a5f5b2
Chethan Kumar <chethankumars.v2016@gmail.com>
2024-03-13 00:02:44 +0530

    added JSX

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+const heading = React.createElement('h1', { id: "heading" }, "Namaste React")
 
-const parent = React.createElement('div', { id: 'parent' }, [React.createElement('div', { id: "child" }, [React.createElement('h1', {}, "I'm an h1 tag from react.createElement()"), React.createElement('h2', {}, "I'm an h2 tag from react.createElement()")]),
-React.createElement('div', { id: "child2" }, [React.createElement('h1', {}, "I'm an h1 tag from react.createElement()"), React.createElement('h2', {}, "I'm an h2 tag from react.createElement()")])])
+const root = ReactDOM.createRoot(document.getElementById("root"))
 
+const jsxheading = (<h1 id='heading'>Namaste React using JSX</h1>)
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(parent)
+root.render(jsxheading)
